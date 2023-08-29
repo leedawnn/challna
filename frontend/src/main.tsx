ab8cf41a882633954507f3e7f7332ee5f286a997
peacemaker474 <hc456123@gmail.com>
2023-08-29 22:09:17 +0900

    setting: msw 기본 설정 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App.tsx'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import initializeMockupWorker from './mocks';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+initializeMockupWorker().then(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
