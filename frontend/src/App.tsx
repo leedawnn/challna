8a0bc710e338d5d482a77e5def30b55bec140500
peacemaker474 <hc456123@gmail.com>
2023-08-31 14:51:20 +0900

    test(deploy): Github Action 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Button> hi </Button>
+      <Button> HELLO </Button>
     </>
   );
 }
