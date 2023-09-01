4282d4be191ef68f1717e8161784c8200d84b560
leedawnn <2g_hee@naver.com>
2023-09-02 00:39:40 +0900

    setting: react-query 초기 세팅

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,28 @@ import initializeMockupWorker from './mocks';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyles.ts';
 import router from './router/index.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      suspense: true,
+      useErrorBoundary: true,
+    },
+  },
+});
 
 initializeMockupWorker().then(() => {
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
-      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>,
   );
 });
