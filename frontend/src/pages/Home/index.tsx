45d0bf19889de88ef88d828c1a3b884ad48ee441
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-09-01 14:27:39 +0900

    test: 모바일 환경 테스트 (#7)

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <div>여기는 홈입니다.</div>;
+  return (
+    <>
+      <div>여기는 홈입니다.</div>
+      <input type="file" accept="image/*"/>
+    </>
+  );
 };
 
 export default HomePage;
