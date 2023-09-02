b091928d239c2a736a969d34c6179193cad4a505
peacemaker474 <hc456123@gmail.com>
2023-09-02 16:58:50 +0900

    refactor(types): 반응형 관련 타입 일부 수정

--- frontend/src/constants/mediaQuery.ts
+++ frontend/src/constants/mediaQuery.ts
@@ -1,4 +1,4 @@
-import SCREEN_BREAKPOINT, { ScreenBreakpoint } from './screenBreakpoints';
+import { SCREEN_BREAKPOINT } from './screenBreakpoints';
 
 /**
  * css에서 반응형 디자인을 할 때 사용합니다.
@@ -20,7 +20,7 @@ import SCREEN_BREAKPOINT, { ScreenBreakpoint } from './screenBreakpoints';
  *   }
  * `
  */
-const MEDIA_QUERY: Readonly<Record<ScreenBreakpoint, string>> = {
+const MEDIA_QUERY: Readonly<Record<keyof typeof SCREEN_BREAKPOINT, string>> = {
   sm: `@media screen and (max-width: ${SCREEN_BREAKPOINT.sm}px)`,
   md: `@media screen and (max-width: ${SCREEN_BREAKPOINT.md}px)`,
   lg: `@media screen and (min-width: ${SCREEN_BREAKPOINT.lg}px)`,
--- frontend/src/constants/screenBreakpoints.ts
+++ frontend/src/constants/screenBreakpoints.ts
@@ -1,9 +1,5 @@
-export type ScreenBreakpoint = 'sm' | 'md' | 'lg';
-
-const SCREEN_BREAKPOINT = {
+export const SCREEN_BREAKPOINT = {
   sm: 375,
   md: 1023,
   lg: 1024,
 } as const;
-
-export default SCREEN_BREAKPOINT;
