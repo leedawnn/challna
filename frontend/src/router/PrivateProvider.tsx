ab7a549fcc774e57721945d9bc98f110541429b8
peacemaker474 <hc456123@gmail.com>
2023-09-05 22:44:54 +0900

    style: 안 쓰는 코드라인 제거

--- frontend/src/router/PrivateProvider.tsx
+++ frontend/src/router/PrivateProvider.tsx
@@ -1,7 +1,6 @@
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import MomentBody from "../components/layout/MomentBody";
-import { Outlet } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
