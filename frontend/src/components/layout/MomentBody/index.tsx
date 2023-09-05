5dd4e617f2f999af3e8cba8f5e3e98b7280a9c22
peacemaker474 <hc456123@gmail.com>
2023-09-05 22:44:07 +0900

    feat: private, public 라우터 추가 및 일부 코드 수정

--- frontend/src/components/layout/MomentBody/index.tsx
+++ frontend/src/components/layout/MomentBody/index.tsx
@@ -1,14 +1,13 @@
 import { styled } from 'styled-components';
-import Header from '../Header';
-import Footer from '../Footer';
-import { Outlet } from 'react-router-dom';
 
-const MomentBody = () => {
+type Props = {
+  children: React.ReactNode;
+}
+
+const MomentBody = ({ children }: Props) => {
   return (
     <Layout>
-      <Header />
-      <Outlet />
-      <Footer />
+      {children}
     </Layout>
   );
 };
@@ -18,7 +17,6 @@ export default MomentBody;
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
   align-items: center;
   width: 36rem;
   height: 100%;
---
+++ frontend/src/router/PrivateProvider.tsx
@@ -0,0 +1,20 @@
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/Header";
+import MomentBody from "../components/layout/MomentBody";
+import { Outlet } from "react-router-dom";
+
+type Props = {
+  children: React.ReactNode;
+}
+
+const PrivateProvider = ({ children }: Props) => {
+  return (
+    <MomentBody>
+      <Header />
+      {children}
+      <Footer />
+    </MomentBody>
+  );
+}
+
+export default PrivateProvider;
