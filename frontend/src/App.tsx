59f43dd091d61813d3a48d776d922af0a717fc44
leedawn <2g_hee@naver.com>
2023-09-05 22:18:57 +0900

    style: 헤더, 푸터 추가 및 기본 레이아웃 설정 (#13)
    * style(layout): 기본 레이아웃 설정
    
    * style(header): 헤더 추가 및 레이아웃 설정
    
    * style(footer): 푸터 추가 및 레이아웃 설정
    
    * refactor: Outlet을 MomentBody 안에서 import하는 것으로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import { Outlet } from 'react-router-dom';
-import Header from './components/layout/Header';
+import { styled } from 'styled-components';
+import MomentBody from './components/layout/MomentBody';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <Layout>
+      <Container>
+        <MomentBody />
+      </Container>
+    </Layout>
   );
 }
 
 export default App;
+
+const Layout = styled.section`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(245, 245, 245);
+`;
+
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
