d14b90e370cd86cf608dc02ecbf98de622f8b52d
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-09-07 13:47:34 +0900

    feat: 로그인 페이지 추가 및 host_01 레이아웃 완료 (#15)
    * style: host_01 레이아웃 개발
    
    * style: host_01 디자인 적용
    
    * style: 카카오 로그인 위아래 마진값 수정
    
    * style: 그라데이션 및 height 값 수정 추가

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,7 +1,55 @@
+import Logo from "../../components/login/Logo";
+import SocialButton from "../../components/login/SocialButton";
+import { styled } from "styled-components";
+
 const LoginPage = () => {
   return (
-    <div> 로그인 페이지 </div>
+    <Container>
+      <LogoWrapper>
+        <Logo />
+      </LogoWrapper>
+      <SocialWrapper>
+        <SocialTitle> 나만의 특별한 추억을 만들어 보세요. </SocialTitle>
+        <SocialButton socialType="kakao" />
+      </SocialWrapper>
+    </Container>
   )
 };
 
 export default LoginPage;
+
+const Container = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  background: linear-gradient(0deg, #66648B 0%, #92B5D9 100%);
+`;
+
+const LogoWrapper = styled.div`
+  width: 100%;
+  height: 80%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SocialWrapper = styled.div`
+  width: 100%;
+  height: 20%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SocialTitle = styled.h3`
+  font-size: 11px;
+  color: #fff;
+  
+  margin-bottom: 12px;
+`;
