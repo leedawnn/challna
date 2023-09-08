aa3b1b815a90b4afa0f0f92b1aa13441d1bfc203
peacemaker474 <hc456123@gmail.com>
2023-09-08 17:54:02 +0900

    feat: 카카오 로그인시 MSW 목업 데이터 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -27,4 +27,23 @@ export const handlers = [
       }),
     );
   }),
+  rest.post('/login/kakao', (req, res, ctx) => {
+    const { code } = req.body;
+
+    if (typeof code === 'string') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          message: "성공하였습니다."
+        })
+      )
+    }
+
+    return res(
+      ctx.status(400),
+      ctx.json({
+        errorMessage: "잘못된 접근입니다."
+      })
+    )
+  })
 ];
--- frontend/src/router/Redirection.tsx
+++ frontend/src/router/Redirection.tsx
@@ -1,12 +1,23 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Redirection = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     const params = new URL(document.location.toString()).searchParams;
     const code = params.get('code');
 
-    console.log(code);
+    const data = {
+      code,
+    }
+
+    axios.post('/login/kakao', data).then((res) => {
+      if (res.status === 200) {
+        navigate('/');
+      }
+    });
     
   }, [])
 
