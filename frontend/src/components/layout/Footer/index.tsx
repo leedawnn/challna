20cf30b3e5dcb2a7de39a74f5d93e2a84eb39e4b
leedawnn <2g_hee@naver.com>
2023-09-10 17:00:17 +0900

    styles: 푸터 초기 디자인 적용

--- frontend/src/components/layout/Footer/index.tsx
+++ frontend/src/components/layout/Footer/index.tsx
@@ -1,9 +1,21 @@
 import { styled } from 'styled-components';
 
 const Footer = () => {
+  const FOOTER_ICONS = []; // 추후 메뉴 아이콘 이미지 배열
+  const FOOTER_ITEMS = ['아이콘', '아이콘', '아이콘'];
+
   return (
     <Layout>
-      <Container>이것은 푸터</Container>
+      <Container>
+        <MenuWrapper>
+          {FOOTER_ITEMS.map((item: string) => (
+            <MenuItem>
+              <MenuIcon />
+              <MenuName>{item}</MenuName>
+            </MenuItem>
+          ))}
+        </MenuWrapper>
+      </Container>
     </Layout>
   );
 };
@@ -17,10 +29,9 @@ const Layout = styled.footer`
 
   max-width: 57.6rem;
   width: 100%;
-  height: 5.7rem;
+  height: 10rem;
 
-  color: #fff;
-  background-color: #292929;
+  background-color: #fff;
 
   position: fixed;
   bottom: 0px;
@@ -30,4 +41,33 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 100%;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 0 4.8rem;
+`;
+
+const MenuItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const MenuIcon = styled.div`
+  width: 4rem;
+  height: 4rem;
+  background-color: #d9d9d9;
+  margin-bottom: 1rem;
+`;
+
+const MenuName = styled.span`
+  color: #d9d9d9;
+  font-size: 1.4rem;
 `;
