4d0d0107b23c8bbd3b3182d47e3e29aacc7ffc8d
peacemaker474 <hc456123@gmail.com>
2023-09-21 21:29:02 +0900

    refactor: 메인 앨범 무한 스크롤 로직 hooks로 분리

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,34 +1,14 @@
-import { AxiosError, AxiosResponse } from 'axios';
-import { MAIN_ALBUM_KEY, getAlbumLists } from '../../api/album';
-
 import HomeAlbum from '../../components/home/HomeAlbum';
 import NoneContent from '../../components/layout/NoneContent';
-import { fetchQuery } from '../../utils/fetchQuery';
 import { useAtomValue } from 'jotai';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import useInfinityAlbum from '../../hooks/useInfinityAlbum';
 import useIntersectionObserver from '../../hooks/useInfinityObserver';
 import { userStore } from '../../stores/userStore';
 
 const HomePage = () => {
   const users = useAtomValue(userStore);
 
-  const {
-    data: albumData,
-    fetchNextPage,
-    hasNextPage,
-  } = useInfiniteQuery<AxiosResponse, AxiosError>(
-    MAIN_ALBUM_KEY,
-    ({ pageParam = 0 }) => getAlbumLists(users?.accessToken as string, fetchQuery({ page: pageParam })),
-    {
-      getNextPageParam: (lastPage) => {
-        return lastPage.data.last ? undefined : lastPage.data.pageNumber + 1;
-      },
-      select: (data) => ({
-        pages: data?.pages.flatMap((page) => page.data.content),
-        pageParams: data.pageParams,
-      }),
-    },
-  );
+  const { data: albumData, fetchNextPage, hasNextPage } = useInfinityAlbum(users?.accessToken ?? '');
 
   const { setTarget } = useIntersectionObserver({
     hasNextPage,
@@ -39,7 +19,7 @@ const HomePage = () => {
     <>
       {albumData ? (
         <>
-          <HomeAlbum data={(albumData?.pages as any[]) || []} />
+          <HomeAlbum data={(albumData?.pages as any[]) ?? []} />
           <div ref={setTarget} />
         </>
       ) : (
