e42f34fcab92a078ea70a7e1f948afef83e1c53e
leedawn <2g_hee@naver.com>
2023-10-04 01:16:03 +0900

    feat: access token 관련 작업 및 권한 분기 (#35)
    * feat: accessToken이 만료되었을 때 refreshToken으로 갈아끼는 로직 구현
    
    * feat: accessToken 관련 작업중
    
    * feat: accessToken 관련 작업중
    
    * chore: 전체적인 패키지 재설치
    
    * refactor: accessToken은 store에만 저장, refreshToken은 localStorage에 저장되게 변경
    
    - Hook 네이밍 변경 : getAccessToken -> useGetAccessToken
    
    * feat: 만료된 토큰을 없애는 로직 추가
    
    * feat: 권한 분기를 위한 withAuth HOC 추가
    
    * refactor: 중복되는 타입 제거 및 기존에 존재하는 타입으로 적용
    
    * chore: withAuth.tsx 폴더 구조 변경

---
+++ frontend/src/hooks/useGetAccessToken.ts
@@ -0,0 +1,43 @@
+import { useAtom, useSetAtom } from 'jotai';
+import { accessTokenStore } from '../stores/accessToken';
+import { useMutation } from '@tanstack/react-query';
+import { IS_VALID_TOKEN, restoreAcccessToken } from '../api/auth';
+import { HTTP_STATUS } from '../constants/api';
+import { atomWithStorage } from 'jotai/utils';
+import { AuthToken } from '../types/user';
+
+export async function useGetAccessToken() {
+  const [accessToken, setAccessToken] = useAtom(accessTokenStore);
+
+  const refreshTokenAtom = atomWithStorage('REFRESH_TOKEN', '');
+  const setRefreshToken = useSetAtom(refreshTokenAtom);
+
+  try {
+    const result = useMutation(
+      IS_VALID_TOKEN,
+      (data: AuthToken) =>
+        restoreAcccessToken({
+          headers: {
+            'X-AUTH-TOKEN': accessToken,
+          },
+          data,
+        }),
+      {
+        onSuccess: (res) => {
+          if (res.status === HTTP_STATUS.OK) {
+            console.log('res: ', res); // 추후 엑세스 토큰이 만료되었을 때, 함수 호출이 잘 되는지 확인 후 제거 필요
+            setAccessToken(res.data?.data.accessToken);
+            setRefreshToken(res.data?.data.refreshToken);
+          }
+        },
+      },
+    );
+
+    const newAccessToken = result.data?.data.refreshToken;
+    return newAccessToken;
+  } catch (error) {
+    localStorage.removeItem('REFRESH_TOKEN');
+    console.error('Refresh Token Error:', error);
+    throw error;
+  }
+}
--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -1,12 +1,12 @@
 export type AuthToken = {
   accessToken: string;
-  accessTokenExpireDate: string;
+  accessTokenExpireDate: Date;
   refreshToken: string;
-}
+};
 
 export type User = AuthToken & {
   kakaoId: number;
   email: string;
   kakaoName: string;
   profile_image: string;
-}
+};
