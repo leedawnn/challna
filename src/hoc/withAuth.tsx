510a0635228a1fbcea1921fd781e70dceef949a9
peacemaker474 <hc456123@gmail.com>
2023-10-06 03:16:21 +0900

    refactor: ESLint 규칙 일부 및 중복 제거, 관련 코드 수정

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -6,7 +6,7 @@ import { accessTokenStore } from '../stores/accessToken';
 // 추후 제네릭으로 선언한 부분에 props로 넘겨주는 값이 있을 경우, 추가 선언 필요
 
 export const withAuth = (Component: React.ComponentType) =>
-  function WithAuthentication<P extends {}>(props: P) {
+  function WithAuthentication<P extends Record<string, unknown>>(props: P) {
     const accessToken = useAtomValue(accessTokenStore);
 
     if (!accessToken) return <Navigate to={ROUTES_PATH.login} />;
