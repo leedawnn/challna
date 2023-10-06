94ac1c26ff40f1a3c9355f55d59bd2f09ce19b2d
peacemaker474 <hc456123@gmail.com>
2023-10-07 02:22:10 +0900

    fix: 처음 앨범 상세로 진입시 앨범 상세 스토어 null 값 에러 해결

--- src/components/home/HomeAlbum.tsx
+++ src/components/home/HomeAlbum.tsx
@@ -1,20 +1,28 @@
 import { Link } from 'react-router-dom';
+import { useSetAtom } from 'jotai';
 import * as S from './HomeAlbum.styled';
 
 import type { Album } from '../../types/album';
 import LazyImage from '../common/LazyImage';
 import { ROUTES_PATH } from '../../constants/routes';
+import { albumDetailStore } from '../../stores/albumDetailStore';
 
 type Props = {
   data: Album[];
 };
 
 const HomeAlbum = ({ data }: Props) => {
+  const setAlbumDetails = useSetAtom(albumDetailStore);
+
+  const handleSaveImageDetail = (album: Album) => () => {
+    setAlbumDetails(album);
+  };
+
   return (
     <S.Container>
       {data?.map((album: Album, index: number) => (
         <S.AlbumWrapper key={album.id || index}>
-          <Link to={ROUTES_PATH.albumDetail} state={{ order: index }}>
+          <Link to={ROUTES_PATH.albumDetail} state={{ order: index }} onClick={handleSaveImageDetail(album)}>
             <LazyImage src={album.resizeUrl} alt={album.originName} />
           </Link>
         </S.AlbumWrapper>
