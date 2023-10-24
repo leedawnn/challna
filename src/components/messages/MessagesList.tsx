1e64c77195cf561ccc9432dd74e5a935a656ef3c
kmin-jeong <desk535353@gmail.com>
2023-10-25 01:52:45 +0900

    feat: 메세지 상세페이지 컴포넌트 생성

--- src/components/messages/MessagesList.tsx
+++ src/components/messages/MessagesList.tsx
@@ -1,16 +1,14 @@
 import { useQueryClient, useQuery, useInfiniteQuery } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse, Axios } from 'axios';
 import { useAtomValue } from 'jotai';
-import { MAIN_MESSAGE_KEY, getBoardList } from '../../api/messages';
-import { fetchQuery } from '../../utils/fetchQuery';
-import { message, messageFront, ListProps } from '../../types/message';
-import instance from '../../api/axios';
+import { message, ListProps } from '../../types/message';
 import { userStore } from '../../stores/userStore';
-import { HTTP_METHOD } from '../../constants/api';
 import { useMessageLists } from '../../hooks/useMessageList';
 import { MessageContainer, Content, DateTime, ContentWrapper } from './Message.styled';
 import { RightIcon } from '../../assets/icons/RightIcon';
 import { Eclipse } from '../../assets/icons/Eclipse';
+import { Link } from 'react-router-dom';
+import { ROUTES_PATH } from '../../constants/routes';
 
 export type Props = {
   data: message[];
@@ -30,21 +28,19 @@ export const Messagelist = ({ data }: ListProps) => {
           };
 
           return (
-            // <MessageContainer key={message.board_id}>
-            //   <Content>{message.content}</Content>
-            //   <DateTime>{message.dateTime}</DateTime>
-            // </MessageContainer>
             <MessageContainer key={message.board_id}>
-              <ContentWrapper>
-                <Content>
-                  <Eclipse />
-                </Content>
-                <Content>{message.content}</Content>
-                <Content>
-                  <RightIcon />
-                </Content>
-              </ContentWrapper>
-              <DateTime>{message.dateTime}</DateTime>
+              <Link to={ROUTES_PATH.messageDetail}>
+                <ContentWrapper>
+                  <Content>
+                    <Eclipse />
+                  </Content>
+                  <Content>{message.content}</Content>
+                  <Content>
+                    <RightIcon />
+                  </Content>
+                </ContentWrapper>
+                <DateTime>{message.dateTime}</DateTime>
+              </Link>
             </MessageContainer>
           );
         })}
@@ -52,22 +48,4 @@ export const Messagelist = ({ data }: ListProps) => {
   );
 };
 
-// export const Messagelist = ({ data }: Props) => {
-//   // const users = useAtomValue(userStore);
-//   // const { data: message, isLoading } = useMessageLists(users?.accessToken as string);
-//   // if (isLoading) return <div>loading</div>;
-//   return (
-//     <div>
-//       {data &&
-//         data?.map((message: message) => (
-//           <div>
-//             {/* <li>{message?.pages}</li> */}
-//             <li>{message.pages.content}</li>
-//             <li>{message.pages.dateTime}</li>
-//           </div>
-//         ))}
-//       jammanbo
-//     </div>
-//   );
-// };
 export default Messagelist;
---
+++ src/pages/MessageDetail/index.tsx
@@ -0,0 +1,11 @@
+import MessageDetail from '../../components/messages/MessageDetail';
+
+const MessageDetailPage = () => {
+  return (
+    <>
+      <MessageDetail />
+    </>
+  );
+};
+
+export default MessageDetailPage;
