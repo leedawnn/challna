6d02cb90d617b1bb991b63010be760a01e6d34f1
kmin-jeong <desk535353@gmail.com>
2023-10-25 01:08:31 +0900

    fix: message page와 messageList분리

--- src/components/messages/MessagesList.tsx
+++ src/components/messages/MessagesList.tsx
@@ -2,19 +2,21 @@ import { useQueryClient, useQuery, useInfiniteQuery } from '@tanstack/react-quer
 import { AxiosError, AxiosResponse, Axios } from 'axios';
 import { MAIN_MESSAGE_KEY, getBoardList } from './../../api/messages';
 import { fetchQuery } from './../../../src/utils/fetchQuery';
-import { message, messageFront } from '.././../types/message';
+import { message, messageFront, ListProps } from '.././../types/message';
 import instance from '../../api/axios';
 import { useAtomValue } from 'jotai';
 import { userStore } from '../../stores/userStore';
 import { HTTP_METHOD } from '../../constants/api';
 import { useMessageLists } from '../../hooks/useMessageList';
-import { MessageContainer, Content, DateTime } from './Message.styled';
+import { MessageContainer, Content, DateTime, ContentWrapper } from '../../components/messages/Message.styled';
+import { RightIcon } from '../../assets/icons/RightIcon';
+import { Eclipse } from '../../assets/icons/Eclipse';
 
 export type Props = {
   data: message[];
 };
 
-export const Messagelist = ({ data }: Props) => {
+export const Messagelist = ({ data }: ListProps) => {
   const users = useAtomValue(userStore);
   const { data: message } = useMessageLists(users?.accessToken as string);
   return (
@@ -28,8 +30,20 @@ export const Messagelist = ({ data }: Props) => {
           };
 
           return (
+            // <MessageContainer key={message.board_id}>
+            //   <Content>{message.content}</Content>
+            //   <DateTime>{message.dateTime}</DateTime>
+            // </MessageContainer>
             <MessageContainer key={message.board_id}>
-              <Content>{message.content}</Content>
+              <ContentWrapper>
+                <Content>
+                  <Eclipse />
+                </Content>
+                <Content>{message.content}</Content>
+                <Content>
+                  <RightIcon />
+                </Content>
+              </ContentWrapper>
               <DateTime>{message.dateTime}</DateTime>
             </MessageContainer>
           );
--- src/pages/Messages/index.tsx
+++ src/pages/Messages/index.tsx
@@ -6,7 +6,7 @@ import { useQuery } from '@tanstack/react-query';
 import { userStore } from '../../stores/userStore';
 import { HTTP_METHOD } from '../../constants/api';
 import { useMessageLists } from '../../hooks/useMessageList';
-import { message } from '../../types/message';
+import { message, messageFront, ListProps } from '../../types/message';
 import { MessageContainer, Content, DateTime, ContentWrapper } from '../../components/messages/Message.styled';
 import { RightIcon } from '../../assets/icons/RightIcon';
 import { Eclipse } from '../../assets/icons/Eclipse';
@@ -14,47 +14,7 @@ const Messages = () => {
   const users = useAtomValue(userStore);
   const { data: message, isLoading } = useMessageLists(users?.accessToken as string);
   if (isLoading) return <div>loading</div>;
-  // const d = message?.pages.map((content) => content);
-  // console.log(message?.pages.map((item) => item));
-  // console.log(message?.pages.map(content));
-  // console.log(message?.pages.map(content));
-  // return (
-  // <div>
-  //   <div>jammanno</div>
-  // </div>
-  // <>
-  //   {/* {JSON.stringify({ message })} */}
-  //   {/* <Messagelist data={message} /> */}
-  // <Messagelist data={[]} />
-  // </>
-  // );
-  return (
-    <article>
-      {message &&
-        message.pages.map((data: any) => {
-          const message: message = {
-            board_id: data.board_id,
-            dateTime: data.dateTime,
-            content: data.content,
-          };
-
-          return (
-            <MessageContainer key={message.board_id}>
-              <ContentWrapper>
-                <Content>
-                  <Eclipse />
-                </Content>
-                <Content>{message.content}</Content>
-                <Content>
-                  <RightIcon />
-                </Content>
-              </ContentWrapper>
-              <DateTime>{message.dateTime}</DateTime>
-            </MessageContainer>
-          );
-        })}
-    </article>
-  );
+  return <Messagelist data={message?.pages as any[]} />;
 };
 
 export default Messages;
