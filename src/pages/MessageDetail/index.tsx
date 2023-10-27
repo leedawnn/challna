329b5de566fb86892aa6a077b7973280ee6588b6
kmin-jeong <desk535353@gmail.com>
2023-10-27 22:51:15 +0900

    fix: api에서 호출된 이미지 띄우기

--- src/pages/MessageDetail/index.tsx
+++ src/pages/MessageDetail/index.tsx
@@ -1,7 +1,13 @@
+import { useAtomValue } from 'jotai';
 import MessageDetail from '../../components/messages/MessageDetail';
+import { userStore } from '../../stores/userStore';
+import { useMessageLists } from '../../hooks/useMessageList';
 
 const MessageDetailPage = () => {
-  return <MessageDetail />;
+  const users = useAtomValue(userStore);
+  const { data: message } = useMessageLists(users?.accessToken as string);
+
+  return <MessageDetail contentList={message?.pages as any[]} boardImageViewDtoList={message?.pages as any[]} />;
 };
 
 export default MessageDetailPage;
--- src/types/message.ts
+++ src/types/message.ts
@@ -2,8 +2,19 @@ export type message = {
   board_id: any;
   dateTime: string | null;
   content: string | null;
+  boardImageViewDtoList: boardImageViewDtoList;
+  accessUrl: any;
+  originName: any;
 };
 
-export type messageDetail = {
-  messageDetail: message;
+// // 디테일 페이지 이메일 타입들
+export type boardImageViewDtoList = {
+  imageId: number | null;
+  dateTime: string | null;
+  accessUrl: any;
+  originName: any;
+  board_id: number | null;
+  metaDateTime: string;
+  resizeUrl: string;
+  [key: string]: any;
 };
