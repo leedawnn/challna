761a5914a51e996c98a235810f7df4ed698e8e07
kmin-jeong <desk535353@gmail.com>
2023-10-29 16:06:49 +0900

    fix: pagination 클릭안되던거 해결

--- src/components/messages/MessageDetail.tsx
+++ src/components/messages/MessageDetail.tsx
@@ -1,23 +1,41 @@
 import { useLocation } from 'react-router-dom';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Container } from './Messagedetail.styled';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './MessageSwiper.css';
 
 const MessageDetail = () => {
   const location = useLocation();
   const ImageItem = location.state.boardImageViewDtoList;
   return (
-    <div>
-      <div>{location.state.board_id}</div>
-      <div>{location.state.content}</div>
-      <div>{location.state.dateTime}</div>
-      <div>
+    <Container>
+      <Swiper
+        spaceBetween={10}
+        pagination={{ clickable: true, el: '.swiper-pagination' }}
+        modules={[Pagination]}
+        className="mainSwiper"
+      >
         {ImageItem?.map((List: any) => {
           return (
-            <div key={List.ImageId}>
-              <img src={List.accessUrl} alt={List.originName} style={{ width: '500px', height: '375px' }} />
+            <div>
+              <SwiperSlide>
+                <img
+                  src={List.accessUrl}
+                  alt={List.originName}
+                  style={{ width: '500px', height: '375px', alignItems: 'center' }}
+                />
+              </SwiperSlide>
+              {/* <div className="swiper-pagination"></div> */}
             </div>
           );
         })}
-      </div>
-    </div>
+      </Swiper>
+      <div className="swiper-pagination" />
+      <div>{location.state.content}</div>
+      <div>{location.state.dateTime}</div>
+    </Container>
   );
 };
 
---
+++ src/components/messages/Messagedetail.styled.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+// 페이지 최상위 컨테이너
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
