e89b53bbad37247a5480626a6bfe6307f3f4fd06
peacemaker474 <hc456123@gmail.com>
2023-10-31 10:44:45 +0900

    style: 프로필 게시글 관련 레이아웃 추가

--- src/components/mypage/Profile.styled.ts
+++ src/components/mypage/Profile.styled.ts
@@ -10,6 +10,12 @@ export const Container = styled.div`
 
 export const ProfileWrapper = styled.div`
   display: flex;
+  align-items: center;
+  gap: 60px;
+
+  & > div {
+    flex-grow: 1;
+  }
 `;
 
 export const ProfileImage = styled.img`
@@ -17,4 +23,25 @@ export const ProfileImage = styled.img`
   height: 80px;
 
   border-radius: 50%;
+  box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.2);
+`;
+
+export const ContnetCounterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3px;
+
+  box-sizing: border-box;
+`;
+
+export const CountText = styled.span`
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.point};
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const CountTitleText = styled.span`
+  font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  font-family: ${({ theme }) => theme.typography.body1.fontFamily};
 `;
--- src/components/mypage/Profile.tsx
+++ src/components/mypage/Profile.tsx
@@ -10,6 +10,14 @@ const Profile = () => {
     <S.Container>
       <S.ProfileWrapper>
         <S.ProfileImage src={user.profile_image} alt={`${user.kakaoName}_profile_image`} />
+        <S.ContnetCounterWrapper>
+          <S.CountText> 0 </S.CountText>
+          <S.CountTitleText> 메시지 </S.CountTitleText>
+        </S.ContnetCounterWrapper>
+        <S.ContnetCounterWrapper>
+          <S.CountText> 0 </S.CountText>
+          <S.CountTitleText> 이미지 </S.CountTitleText>
+        </S.ContnetCounterWrapper>
       </S.ProfileWrapper>
     </S.Container>
   );
