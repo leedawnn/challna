60985ccc974079d68bfc2938c3d5da6e839c5830
peacemaker474 <hc456123@gmail.com>
2023-11-02 23:28:18 +0900

    feat: 프로필 편집 레이아웃 완성

--- src/components/common/Modal/template/UpdateProfileModal.tsx
+++ src/components/common/Modal/template/UpdateProfileModal.tsx
@@ -27,6 +27,14 @@ const UpdateProfileModal = () => {
           <ProfileUploadInput type="file" id="profileImage" accept="image/*" />
         </ProfileUploadForm>
       </ProfileImageWrapper>
+      <ProfileNameWrapper>
+        <ProfileNameLabel htmlFor="profileName"> 이름을 정해주세요. </ProfileNameLabel>
+        <ProfileNameInput type="text" />
+      </ProfileNameWrapper>
+      <ButtonWrapper>
+        <SaveButton type="button"> 저장 </SaveButton>
+        <CancelButton type="button"> 취소</CancelButton>
+      </ButtonWrapper>
     </S.MyPageModalContainer>
   );
 };
@@ -83,8 +91,84 @@ const ProfileImage = styled.img`
   border-radius: 50%;
 
   object-fit: contain;
+
+  box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.2);
 `;
 
 const ProfileUploadInput = styled.input`
   display: none;
+`;
+
+const ProfileNameWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+
+  margin-top: 38px;
+`;
+
+const ProfileNameLabel = styled.label`
+  font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  font-family: ${({ theme }) => theme.typography.body1.fontFamily};
+  color: ${({ theme }) => theme.colors.gray1};
+`;
+
+const ProfileNameInput = styled.input`
+  all: unset;
+
+  width: 100%;
+  height: 40px;
+
+  padding-left: 12px;
+  box-sizing: border-box;
+
+  background-color: ${({ theme }) => theme.colors.lightGray};
+
+  border: 1px solid ${({ theme }) => theme.colors.lightGray3};
+  border-radius: 5px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-top: 40px;
+`;
+
+const SaveButton = styled.button`
+  all: unset;
+
+  width: 100%;
+  height: 40px;
+
+  text-align: center;
+
+  border-style: none;
+  border-radius: 5px;
+  box-shadow: 0px 4px 12px 0px rgba(146, 181, 217, 0.5);
+  background-color: ${({ theme }) => theme.colors.point};
+
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.typography.subHead};
+  font-family: ${({ theme }) => theme.typography.subHead};
+  line-height: 40px;
+
+  cursor: pointer;
+`;
+
+const CancelButton = styled.button`
+  all: unset;
+
+  width: 100%;
+  height: 40px;
+
+  text-align: center;
+  text-decoration: underline;
+
+  font-size: ${({ theme }) => theme.typography.subHead};
+  font-family: ${({ theme }) => theme.typography.subHead};
+
+  cursor: pointer;
 `;
