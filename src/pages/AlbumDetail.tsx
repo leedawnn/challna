5a909f730220445fedcb05ef72956920baefdd22
peacemaker474 <hc456123@gmail.com>
2023-11-10 20:49:39 +0900

    refactor: 크롬 미니앨범뷰 수정 테스트

--- src/pages/AlbumDetail.tsx
+++ src/pages/AlbumDetail.tsx
@@ -1,13 +1,21 @@
+import styled from 'styled-components';
 import AlbumDeatilImage from '../components/albumDetail/AlbumDetailImage';
 import AlbumDetailContent from '../components/albumDetail/AlbumDetailContent';
 
 const AlbumDetailPage = () => {
   return (
-    <>
+    <Layout>
       <AlbumDeatilImage />
       <AlbumDetailContent />
-    </>
+    </Layout>
   );
 };
 
 export default AlbumDetailPage;
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  box-sizing: border-box;
+`;
