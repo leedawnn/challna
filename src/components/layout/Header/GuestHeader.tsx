d15a3363c388e34faedcdb13ac39adc47b58a5b0
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-11-13 22:15:30 +0900

    feat: 게스트 페이지 내 공통 헤더 추가 (#76)

---
+++ src/components/layout/Header/GuestHeader.tsx
@@ -0,0 +1,44 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import styled from 'styled-components';
+import BackIcon from '../../../assets/icons/BackIcon';
+import MoreIcon from '../../../assets/icons/MoreIcon';
+import { ROUTES_PATH } from '../../../constants/routes';
+
+const GuestHeader = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleBackToUrl = () => {
+    navigate(-1);
+  };
+
+  return (
+    <Container>
+      <BackIcon onClick={handleBackToUrl} />
+      <Title> 새 게시물 </Title>
+      <MoreIcon style={{ visibility: pathname === ROUTES_PATH.guestEdit ? 'hidden' : 'visible' }} />
+    </Container>
+  );
+};
+
+export default GuestHeader;
+
+const Container = styled.header`
+  width: 100%;
+  height: 6rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 16px 20px;
+  box-sizing: border-box;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors.lightGray2};
+`;
+
+const Title = styled.h3`
+  font-size: ${({ theme }) => theme.typography.subHead3.fontSize};
+  font-family: ${({ theme }) => theme.typography.subHead3.fontFamily};
+`;
--- src/pages/GuestEdit.tsx
+++ src/pages/GuestEdit.tsx
@@ -1,5 +1,30 @@
+import styled from 'styled-components';
+import GuestHeader from '../components/layout/Header/GuestHeader';
+
 const GuestEditPage = () => {
-  return <div> 게스트 수정 페이지 </div>;
+  return (
+    <Layout>
+      <Container>
+        <GuestHeader />
+      </Container>
+    </Layout>
+  );
 };
 
 export default GuestEditPage;
+
+const Layout = styled.div`
+  width: 57.6rem;
+  position: relative;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+
+const Container = styled.main`
+  width: 100%;
+  max-width: 100vw;
+  min-height: calc(var(--vh, 1vh) * 100);
+
+  background-color: ${({ theme }) => theme.colors.white};
+  box-sizing: border-box;
+`;
