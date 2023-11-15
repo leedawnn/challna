b98304e39708ee78118d40a00b6dabb5192fbf1f
peacemaker474 <hc456123@gmail.com>
2023-11-15 20:52:51 +0900

    feat: 게스트 리뷰 페이지 내 캐러셀 추가

--- src/pages/GuestReview.tsx
+++ src/pages/GuestReview.tsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import GuestHeader from '../components/layout/Header/GuestHeader';
+import GuestReviewSwiper from '../components/guestReview/GuestReviewSwiper';
 
 const GuestReviewPage = () => {
   return (
     <Layout>
       <Container>
         <GuestHeader />
+        <GuestReviewSwiper />
       </Container>
     </Layout>
   );
