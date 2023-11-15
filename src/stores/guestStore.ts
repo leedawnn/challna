2296c418d749fc0370839f6228111c953acb37a3
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2023-11-15 20:23:59 +0900

    refactor: 게스트 수정 페이지 스타일 및 스토어 수정 (#79)
    * refactor: 게스트 편집 페이지 캐러셀 영역 다음 사진 보이게 수정
    
    * style: 이미지 사이즈 옵션 변경
    
    * refactor: 게스트 앨범 스토어 수정
    
    * feat: 게스트 수정 페이지 내 완료 로직 추가

--- src/stores/guestStore.ts
+++ src/stores/guestStore.ts
@@ -2,6 +2,11 @@ import { atom } from 'jotai';
 import type { Guest } from '../types/user';
 import type { GuestAlbum } from '../types/album';
 
-export const guestFileStore = atom<GuestAlbum[] | null>(null);
+export const initialAlbumStore = {
+  images: null,
+  message: '',
+};
+
+export const guestAlbumStore = atom<GuestAlbum>(initialAlbumStore);
 
 export const guestAuthStore = atom<Guest | null>(null);
